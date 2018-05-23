b18a2e3dcdb24ae6c27d7d906cabf4985b961db1
Sam W <chicagomike999@gmail.com>
2018-05-23 17:37:48 -0400

    some last bits of stuff
    we're moving on now, but I may still test things here I found a better testing enviroment.

--- js/BulbScript.js
+++ js/BulbScript.js
@@ -1,3 +1,5 @@
+var test2 = "Oh thank god."
 function bulbOff() {
     document.getElementById("myImage").src = "imgs/pic_bulboff.gif";
+    document.getElementById("paragraph1Text").innerHTML = test2;
 }
